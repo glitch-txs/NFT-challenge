c8f3b8eb6980034c60413aadbc98bc35d5044db9
Glitch <glitch.txs@gmail.com>
2023-03-06 22:07:02 -0300

    progress

--- src/pages/api/getNFTs.ts
+++ src/pages/api/getNFTs.ts
@@ -10,8 +10,8 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
 
-  const { contractAddress } = req
-  0x59325733eb952a92e069C87F0A6168b29E80627f
+  const { contractAddress } = req.query
+  // 0x59325733eb952a92e069C87F0A6168b29E80627f
   const {data} = await axios.get(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}/getNFTsForCollection?contractAddress=${contractAddress}&withMetadata=true`)
   res.status(200).json(data)
 }
